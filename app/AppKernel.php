ca9a203cb73326b361c33cdce598f9707b6b4184
smachara <smachara@gmail.com>
2016-03-23 16:56:06 +0100

    It is not working I'm still fixed.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new PdfBundle\PdfBundle(),
-            new smachara\Html2pdfBundle\smacharaHtml2pdfBundle(),
+            new smachara\html2pdfBundle\Tests\Controller\DefaultControllerTest(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
